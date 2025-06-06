c491b24b0cdbf757555b7579e7b8006ce26269c6
Cezar8910 <cezardeaguiargalvao@gmail.com>
2025-06-06 16:45:37 -0300

    update code

--- aps1/aps1.asm
+++ aps1/aps1.asm
@@ -1,72 +1,90 @@
-; Programa em Assembly x86 modo real para contar vogais em uma string
-; Montador: MASM/TASM/NASM (com pequenas adaptações)
-; Executa em DOS
+; Bootloader Assembly - Conta vogais em uma string e mostra na tela
+ORG 0x7C00
+BITS 16
 
-org 100h
+    jmp start
 
-section .data
-    msg db 'Exemplo de string para contar vogais.', 0
-    vogais db 'AEIOUaeiou'
-    resultado db 'Quantidade de vogais: ', 0
-    contagem db 0
+msg:        db "Exemplo de string para contar vogais.", 0
+vogais:     db "AEIOUaeiou", 0
+resultado:  db "Qtd de vogais: ", 0
 
-section .text
 start:
-    mov si, msg          ; SI aponta para o início da string
-    mov bl, 0            ; BL = contador de vogais
+    xor ax, ax
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    mov sp, 0x7C00  ; stack segura
 
-proximo_caractere:
-    mov al, [si]         ; Pega caractere atual
-    cmp al, 0
-    je fim_string        ; Fim da string
-
-    ; Verifica se é vogal
-    mov di, vogais
-    mov cx, 10           ; 10 vogais
-verifica_vogal:
-    mov ah, [di]
-    cmp al, ah
-    je achou_vogal
-    inc di
-    loop verifica_vogal
-    jmp nao_vogal
-
-achou_vogal:
-    inc bl               ; Incrementa contador
-
-nao_vogal:
-    inc si
-    jmp proximo_caractere
-
-fim_string:
-    mov [contagem], bl
-
-    ; Exibe mensagem "Quantidade de vogais: "
-    mov dx, resultado
+    ; Exibe mensagem
+    mov si, resultado
     call print_string
 
-    ; Converte BL (contador) para ASCII e exibe
-    mov al, bl
-    add al, '0'
-    mov ah, 0Eh
-    int 10h
+    ; Conta vogais
+    mov si, msg
+    xor bx, bx      ; contador de vogais = 0
 
-    ; Fim do programa
-    mov ah, 4Ch
-    int 21h
+.proximo_caractere:
+    lodsb
+    or al, al
+    jz .fim_contagem
 
-;----------------------------------------
-; Rotina para imprimir string em DX
+    push si
+    mov di, vogais
+    mov cx, 10
+.verifica_vogal:
+    mov ah, [di]
+    cmp al, ah
+    je .achou_vogal
+    inc di
+    loop .verifica_vogal
+    jmp .nao_vogal
+
+.achou_vogal:
+    inc bl
+
+.nao_vogal:
+    pop si
+    jmp .proximo_caractere
+
+.fim_contagem:
+    ; Mostra contador
+    mov ax, bx
+    call print_number
+
+halt:
+    jmp $
+
+; Exibe string terminada em 0
 print_string:
-    pusha
-    mov ah, 0Eh
-.next_char:
-    mov al, [dx]
-    cmp al, 0
-    je .fim
-    int 10h
-    inc dx
-    jmp .next_char
-.fim:
-    popa
+.loop:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp .loop
+.done:
     ret
+
+; Exibe número em AX
+print_number:
+    mov bx, 10
+    mov cx, 0
+.loop1:
+    xor dx, dx
+    div bx
+    add dl, '0'
+    push dx
+    inc cx
+    cmp ax, 0
+    jne .loop1
+.loop2:
+    pop ax
+    mov ah, 0x0E
+    int 0x10
+    loop .loop2
+    ret
+
+; Boot signature (obrigatória para BIOS)
+times 510 - ($ - $$) db 0
+dw 0xAA55
