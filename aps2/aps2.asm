5204e89a80a6f3144f38e687387f2b8f18f877dc
Cezar8910 <cezardeaguiargalvao@gmail.com>
2025-06-12 15:32:37 -0300

    update aps2

--- aps2/aps2.asm
+++ aps2/aps2.asm
@@ -1,103 +1,126 @@
 ; diretivas para o assembler
+
 org 0x7C00
 bits 16
 
 jmp start
 
 ; Dados
+
 hello db "hello world", 0x0D, 0x0A, 0
 msg   db "escreva algo: ", 0
 fim   db "fim do programa",0x0D,0xA, 0
-buffer times 64 db 0  ; buffer para entrada do teclado
+buffer times 80 db 0  ; buffer p entrada do teclado
 
 ; inicio do programa
+
 start:
+
 configurando_ivt:
     push ds
     xor ax, ax
-    mov ds, ax ;; garantir DS = 0
+    mov ds, ax ;; por garantia, pode ds != 0
 
-    mov word[0x100], interrupt_40h      ; offset
-    mov word[0x102], 0x0000             ; segmento
+    ; interrupção 40h >> endereço na IVT: 40h * 4 = 100h
+    mov di, 0x100
+    mov word[di], print_string
+    mov word[di+2], 0
 
-    pop ds ;; restaurar DS antigo
+    pop ds ;; devolver o ds antigo
 
-    ;; imprimir "hello world"
     mov ax, hello
     push ax
-    int 0x40
+    int 0x40    ; int 0x40 chama a rotina print_string 
     add sp, 2
 
-    ;; imprimir mensagem
     mov ax, msg
+
     push ax
     int 0x40
     add sp, 2
 
-    ;; ler string do teclado para o buffer
-    mov di, buffer
-    call get_keyboard_input
-
-    ;; chamar int 40h com offset do buffer
-    mov ax, buffer
+    call get_keyboard_input     ; chamando funcao
+    mov ax, buffer              ; printar string
     push ax
     int 0x40
     add sp, 2
 
-    ;; imprimir fim
+end:
     mov ax, fim
     push ax
+    ;;call print_string
     int 0x40
     add sp, 2
-
-fim_loop:
     jmp $ ; halt
 
-; -------------------
-; Rotina da INT 40h
-interrupt_40h:
-    call print_string
-    iret
+; Funções
 
-; -------------------
-; Lê string do teclado até ENTER
+;; agora salva string no buffer
 get_keyboard_input:
     pusha
-.getchar:
+    mov di, buffer ; inicio do buffer
+.loop:
     mov ah, 0x00
-    int 0x16         ; esperar por tecla
-    cmp al, 0x0D
+    int 0x16
+    ;; apartir daqui o char vai ta no al
+    cmp al, 0x0D ; enter
     je .done
-    mov [di], al     ; salvar no buffer
-    inc di
 
-    mov ah, 0x0E     ; ecoar caractere
+    stosb ;  salva al no buffer
+
+    mov ah, 0x0E
     int 0x10
-    jmp .getchar
+    jmp .loop
 .done:
-    mov byte [di], 0 ; null terminador
+    mov al, 0  ; null terminator
+    stosb
     popa
-    ret
+    iret
 
-; -------------------
-; Imprime string apontada por [bp+8]
 print_string:
-    push bp
+    push bp ; salvar o valor de bp
     mov bp, sp
-    mov si, [bp+8]
+    ;; bp+0 -> bp antigo
+    ;; bp+2 -> offset de retorno print_string
+    ;; bp+4 -> segmento de retorno print_string
+    ;; bp+6 -> flags print_string
+    ;; bp+8 -> primeiro parametro
+    mov ax, [bp+8]
+    mov si, ax
 
-.print_loop:
+.loop:
+    ;; carrega do si um byte em ax
     lodsb
     or al, al
     jz .done
     mov ah, 0x0E
     int 0x10
-    jmp .print_loop
+    jmp .loop
 .done:
-    pop bp
+    pop bp ;; devolver o valor antigo de bp
+    iret  ;; n parametros * 2 (modo real)
+
+print_number:
+    pusha
+    mov bx, 10
+    mov cx, 0
+.loop1:
+    mov dx, 0
+    div bx
+    add dx, 48
+    push dx
+    inc cx
+    cmp ax, 0
+    jne .loop1
+.loop2:
+    pop ax
+    mov ah, 0x0E
+    int 0x10
+    loop .loop2
+    popa
     ret
 
-; -------------------
 ; Assinatura do boot sector
-times 510-($-$$) db 0
-dw 0xAA55
+
+    times 510-($-$$) db 0
+    dw 0xAA55
